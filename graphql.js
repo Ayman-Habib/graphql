46dce44d396779eb54c32fabac9cd457fea01466
ayhabib <aymanhabib369@gmail.com>
2026-02-18 22:42:43 +0300

    fixed

--- graphql.js
+++ graphql.js
@@ -330,18 +330,16 @@ class GraphQLService {
         const query = `
             query GetUserLevel($userId: Int!) {
                 transaction(
-                    where: { 
-                        userId: { _eq: $userId },
-                        type: { _eq: "level" },
-                      	path: {_like: "/bahrain/bh-module%"}
-
-                    },
-                    order_by: { createdAt: desc },
-                    limit: 1
-                ) {
-                    amount
-                    createdAt
-                }
+                order_by: {amount: desc}
+                limit: 1
+                where: {
+                type: {_eq: "level"},
+                path: {_like: "/bahrain/bh-module%"}
+            }
+        ) {
+            amount
+        }
+    }
             }
         `;
         
